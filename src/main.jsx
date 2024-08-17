59c5b69c6d9c6af178bf034157b86b01717dddc8
SAJID-AFNAN <sajidafnan91@gmail.com>
2024-08-18 00:32:45 +0600

    Add login page

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,20 @@ import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Root from './Pages/Root/Root.jsx'
 import Error from './Pages/Error/Error.jsx'
-// import Login from './Pages/Login/Login.jsx'
+import Login from './Pages/Login/Login.jsx'
 // import SignUp from './Components/SignUp/SignUp.jsx'
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root></Root>,
-    errorElement: <Error></Error>
+    errorElement: <Error></Error>,
+    children: [
+      {
+        path:"/login",
+        element: <Login></Login>
+      }
+    ]
   }
 ])
 
