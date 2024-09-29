7935e1e3556ccad9a96926ff45eaa9bc25d7a599
SAJID-AFNAN <sajidafnan91@gmail.com>
2024-09-29 11:36:49 +0600

    Add modal as a  trial

--- src/Pages/Apply/Civil/Civil.jsx
+++ src/Pages/Apply/Civil/Civil.jsx
@@ -1,30 +1,70 @@
+import { useState } from "react";
+
+import {
+    Button,
+    Dialog,
+    DialogBody,
+    DialogFooter,
+} from "@material-tailwind/react";
+
 const Civil = () => {
+    const [Request, SetRequest] = useState(false)
+
+    const handleRequest = () => {
+        SetRequest(!Request)
+    }
+
     return (
         <div>
             <h1 className="text-center text-xl bg-indigo-200 p-4 rounded-lg text-black">Department of Civil Engineering</h1>
             <div className="border-2 rounded-xl py-3 px-6  flex justify-between my-4">
                 <p><span className="text-2xl font-semibold font-serif">Professor Dr. Md Mizanur Rahman</span> <br /> <span className="font-sans text-lg">Professor</span></p>
-                <button className="btn btn-success text-lg">Request</button>
+                <Button className="btn hover:text-black">Request</Button>
             </div>
             <div className="border-2 rounded-xl py-3 px-6  flex justify-between my-4">
                 <p><span className="text-2xl font-semibold font-serif">Raihan Khan Opu</span> <br /> <span className="font-sans text-lg">Assistant Teacher and Head of Department</span></p>
-                <button className="btn btn-success text-lg">Request</button>
+                <Button className="btn hover:text-black">Request</Button>
             </div>
             <div className="border-2  rounded-xl py-3 px-6  flex justify-between my-4">
                 <p><span className="text-2xl font-semibold font-serif">Md Akhlas Uddin</span> <br /> <span className="font-sans text-lg">Lecturer</span></p>
-                <button className="btn btn-success text-lg">Request</button>
+                <Button className="btn hover:text-black">Request</Button>
             </div>
             <div className="border-2  rounded-xl py-3 px-6  flex justify-between my-4">
                 <p><span className="text-2xl font-semibold font-serif">Sohanur Rahman</span> <br /> <span className="font-sans text-lg">Lecturer</span></p>
-                <button className="btn btn-success text-lg">Request</button>
+                <Button className="btn hover:text-black">Request</Button>
             </div>
             <div className="border-2  rounded-xl py-3 px-6  flex justify-between my-4">
                 <p><span className="text-2xl font-semibold font-serif">Shahed Mahmud</span> <br /> <span className="font-sans text-lg">Workshop/Laboratory Incharge</span></p>
-                <button className="btn btn-success text-lg">Request</button>
+                <Button className="btn hover:text-black">Request</Button>
             </div>
             <div className="border-2  rounded-xl py-3 px-6  flex justify-between my-4">
                 <p><span className="text-2xl font-semibold font-serif">Puja Brommo</span> <br /> <span className="font-sans text-lg">Guest Lecturer</span></p>
-                <button className="btn btn-success text-lg">Request</button>
+                <Button onClick={handleRequest} className="btn hover:text-black">{Request ? 'Pending...' : 'Request'}</Button>
+                <Dialog
+                    open={Request}
+                    handler={handleRequest}
+                    animate={{
+                        mount: { scale: 1, y: 0 },
+                        unmount: { scale: 0.9, y: -100 },
+                    }}>
+
+                    <DialogBody className="text-4xl text-center">
+                        Are you sure to sent a request ?
+                    </DialogBody>
+                    <DialogFooter>
+                        <Button
+                            variant="text"
+                            color="red"
+                            onClick={handleRequest}
+                            className="mr-1"
+                        >
+                            <span>Cancel</span>
+                        </Button>
+                        <Button variant="gradient" color="green" onClick={handleRequest}>
+                            <span>Confirm</span>
+                        </Button>
+                    </DialogFooter>
+                </Dialog>
             </div>
         </div>
     );
--- src/Pages/Apply/NonTech/NonTech.jsx
+++ src/Pages/Apply/NonTech/NonTech.jsx
@@ -1,38 +1,40 @@
+import { Button } from "@material-tailwind/react";
+
 const NonTech = () => {
     return (
         <div>
             <h1 className="text-center text-xl bg-green-300 p-4 rounded-lg text-black">Department of NonTech or General Science</h1>
             <div className="border-2 rounded-xl py-3 px-6  flex justify-between">
                 <p><span className="text-2xl font-semibold font-serif">Md. Eklachur Rahman</span> <br /> <span className="font-sans text-lg">Associate Professor (Chemistry) & Head of Department Non-Tech</span></p>
-                <button className="btn btn-success text-lg">Request</button>
+                <Button className="btn hover:text-black">Request</Button>
             </div>
             <div className="border-2 rounded-xl py-3 px-6  flex justify-between">
                 <p><span className="text-2xl font-semibold font-serif">Abdus Chatter Mia</span> <br /> <span className="font-sans text-lg">Associate Professor, Physics and Officer on Special Charge, Maushi, Dhaka</span></p>
-                <button className="btn btn-success text-lg">Request</button>
+                <Button className="btn hover:text-black">Request</Button>
             </div>
             <div className="border-2 rounded-xl py-3 px-6  flex justify-between">
                 <p><span className="text-2xl font-semibold font-serif">Muhammad Yunus Ali</span> <br /> <span className="font-sans text-lg">Associate Professor, Mathematics and Officer on Special Charge, Maushi, Dhaka.</span></p>
-                <button className="btn btn-success text-lg">Request</button>
+                <Button className="btn hover:text-black">Request</Button>
             </div>
             <div className="border-2 rounded-xl py-3 px-6  flex justify-between">
                 <p><span className="text-2xl font-semibold font-serif">Md Shahjamal Molya</span> <br /> <span className="font-sans text-lg">Assistant Professor (Chemistry)</span></p>
-                <button className="btn btn-success text-lg">Request</button>
+                <Button className="btn hover:text-black">Request</Button>
             </div>
             <div className="border-2 rounded-xl py-3 px-6  flex justify-between">
                 <p><span className="text-2xl font-semibold font-serif">Jannatun Naeem</span> <br /> <span className="font-sans text-lg">Assistant Professor (Mathematics)</span></p>
-                <button className="btn btn-success text-lg">Request</button>
+                <Button className="btn hover:text-black">Request</Button>
             </div>
             <div className="border-2 rounded-xl py-3 px-6  flex justify-between">
                 <p><span className="text-2xl font-semibold font-serif">Afia Begum</span> <br /> <span className="font-sans text-lg">Lecturer (Physics)</span></p>
-                <button className="btn btn-success text-lg">Request</button>
+                <Button className="btn hover:text-black">Request</Button>
             </div>
             <div className="border-2 rounded-xl py-3 px-6  flex justify-between">
                 <p><span className="text-2xl font-semibold font-serif">Md. Fahad Hossain</span> <br /> <span className="font-sans text-lg">Lecturer (Mathematics)</span></p>
-                <button className="btn btn-success text-lg">Request</button>
+                <Button className="btn hover:text-black">Request</Button>
             </div>
             <div className="border-2 rounded-xl py-3 px-6  flex justify-between">
                 <p><span className="text-2xl font-semibold font-serif">Shihab Uddin</span> <br /> <span className="font-sans text-lg">Lecturer (Chemistry)</span></p>
-                <button className="btn btn-success text-lg">Request</button>
+                <Button className="btn hover:text-black">Request</Button>
             </div>
         </div>
     );
