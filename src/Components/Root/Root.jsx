f685d88fac97e747920775e784b0f46064aa33ba
SAJID-AFNAN <sajidafnan91@gmail.com>
2024-10-06 09:54:00 +0600

    Firebase Deploy

--- src/Components/Root/Root.jsx
+++ src/Components/Root/Root.jsx
@@ -2,12 +2,13 @@ import { Navigate, Outlet } from "react-router-dom";
 import Sidebar from "../Sidebar/Sidebar";
 import { useContext } from "react";
 import { AuthContext } from "../../Providers/AuthProvider";
+import { Spinner } from "@material-tailwind/react";
 
 const Root = () => {
     const { user, loading } = useContext(AuthContext)
 
     if (loading) {
-        return <span className="loading loading-spinner text-accent"></span>
+        return <Spinner className="h-16 w-16 text-gray-900/50" />;
     }
     return (
         <div className="flex p-4">
